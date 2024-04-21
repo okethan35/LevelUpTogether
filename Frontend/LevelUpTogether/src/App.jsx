15fe1cbfa0fbbf2fb20ae8b56166bccdd7c94553
okethan35 <ethantran35@hotmail.com>
2024-04-20 18:31:44 -0700

    addExercise outline

--- Frontend/LevelUpTogether/src/App.jsx
+++ Frontend/LevelUpTogether/src/App.jsx
@@ -2,6 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyCpz8slD9gpJTBf38K7JmtYFpgHcvniLBs",
+  authDomain: "leveluptogether-e7502.firebaseapp.com",
+  projectId: "leveluptogether-e7502",
+  storageBucket: "leveluptogether-e7502.appspot.com",
+  messagingSenderId: "403257475271",
+  appId: "1:403257475271:web:f3c070a6eef7abc6697ee2",
+  measurementId: "G-WKXMXEZZPK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ Frontend/LevelUpTogether/src/addExercise.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore"; 
+const firebaseConfig = {
+  apiKey: "AIzaSyCpz8slD9gpJTBf38K7JmtYFpgHcvniLBs",
+  authDomain: "leveluptogether-e7502.firebaseapp.com",
+  projectId: "leveluptogether-e7502",
+  storageBucket: "leveluptogether-e7502.appspot.com",
+  messagingSenderId: "403257475271",
+  appId: "1:403257475271:web:f3c070a6eef7abc6697ee2",
+  measurementId: "G-WKXMXEZZPK"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+function addExercise() {
+    return(
+
+    )
+}

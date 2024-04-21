80e68cdfa0984d19a7ce40ec5f5ee957a53dbaf5
jkondo14 <jkondo@ucsd.edu>
2024-04-20 17:03:07 -0700

    whats up

--- Frontend/LevelUpTogether/src/App.jsx
+++ Frontend/LevelUpTogether/src/App.jsx
@@ -1,35 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/brandon.jpg'
-import viteLogo from '/brandon.jpg'
+import appLogo from './assets/LevelUp_Together.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='scrolling'>
+      <div className='logo'>
+        <img src={appLogo} alt="Level Up Together Logo" />
       </div>
-      <h1>Welcome to Level Up Together </h1>
+        <h1 className='title'>Welcome! </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <label>
+          First Name: 
+          <br />
+          <input name="userFName" type="text"/>
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <br/>
+          <input name="userFName" type="text"/>
+        </label>
+        <br />
+        <br />
+        <button>
+          Submit
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
+
 
 export default App

e4cf7d70f1422c1ad13b6ec8fc1c54b3ebc20fb2
okethan35 <ethantran35@hotmail.com>
2024-04-20 21:50:50 -0700

    db add func

--- Frontend/LevelUpTogether/src/App.jsx
+++ Frontend/LevelUpTogether/src/App.jsx
@@ -2,6 +2,25 @@ import { useState } from 'react';
 import appLogo from './assets/LevelUp_Together.png'; // Ensure the path is correct
 import './App.css'; // Ensure the path is correct
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore"; 
+import { doc, setDoc } from "firebase/firestore"; 
+const firebaseConfig = {
+  apiKey: "AIzaSyCpz8slD9gpJTBf38K7JmtYFpgHcvniLBs",
+  authDomain: "leveluptogether-e7502.firebaseapp.com",
+  projectId: "leveluptogether-e7502",
+  storageBucket: "leveluptogether-e7502.appspot.com",
+  messagingSenderId: "403257475271",
+  appId: "1:403257475271:web:f3c070a6eef7abc6697ee2",
+  measurementId: "G-WKXMXEZZPK"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+
 function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
@@ -39,14 +58,23 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const submissionData = {};
-    for (const element of event.target.elements) {
-      if (element.name) {
-        submissionData[element.name] = element.value;
-      }
-    }
+    const submissionData = {
+      userFName: firstName,
+      userLName: lastName,
+      selectedWorkouts: selectedWorkouts,
+      reps: reps,
+      sets: sets,
+      date: dateUser
+    };
+
     setLastSubmitted(submissionData); // Store last submission data
     console.log('Submitted:', submissionData); // Ensure console logging is correct
+
+    setDoc(doc(db, "users", "user2", "userData", "" + submissionData.date + "", "weight", "" + submissionData.selectedWorkouts + ""), {
+      sets: submissionData.sets,
+      reps: submissionData.reps
+    });
+
     // Reset form fields
     setFirstName('');
     setLastName('');
@@ -111,8 +139,8 @@ function App() {
       {lastSubmitted && (
         <div>
           <h3>Last Submission:</h3>
-          <p>First Name: {lastSubmitted.firstName}</p>
-          <p>Last Name: {lastSubmitted.lastName}</p>
+          <p>First Name: {lastSubmitted.userFName}</p>
+          <p>Last Name: {lastSubmitted.userLName}</p>
           <p>Workouts: {lastSubmitted.selectedWorkouts ? lastSubmitted.selectedWorkouts.join(', ') : ""}</p>
           <p>Reps: {lastSubmitted.reps}</p> {/* Display the reps in the last submission */}
         </div>
